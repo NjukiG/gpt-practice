1af8154e6e8487d8f6747470c83d2d4149c18421
George <gnjuki19@gmail.com>
2023-09-03 14:15:23 +0300

    Added my api key

--- src/App.js
+++ src/App.js
@@ -11,18 +11,21 @@ import {
 } from "@chatscope/chat-ui-kit-react";
 import { useState } from "react";
 function App() {
+  console.log(process.env.REACT_APP_API_KEY);
+  const API_KEY = process.env.REACT_APP_API_KEY;
   const [messages, setMessages] = useState([
     {
       message: "Hello, I am ChatGPT",
       sender: "ChatGPT",
     },
   ]);
+  const [typing, setTyping] = useState(false);
 
   const handleSend = async (message) => {
     const newMessage = {
       message: message,
       sender: "user",
-      direction: "outgong"
+      direction: "outgoing",
     };
 
     // All the old mesages plus the new one after.
@@ -30,6 +33,9 @@ function App() {
 
     // Update our messages state
     setMessages(newMessages);
+
+    // Set the typing indicator here
+    setTyping(true);
     // Process mesages to chatgpt (send it and see responce)
   };
   return (
@@ -37,7 +43,13 @@ function App() {
       <div style={{ position: "relative", height: "700px", width: "700px" }}>
         <MainContainer>
           <ChatContainer>
-            <MessageList>
+            <MessageList
+              typingIndicator={
+                typing ? (
+                  <TypingIndicator content="ChatGPT is typing responce" />
+                ) : null
+              }
+            >
               {messages.map((message, i) => {
                 return <Message key={i} model={message} />;
               })}
